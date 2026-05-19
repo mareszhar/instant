e67463c4630179b46de24a2d31295c6526129a30
Mares <66750744+mareszhar@users.noreply.github.com>
2026-05-19 01:49:54 -0400

    🎨 clean up tests

--- client/packages/vux/idb-vux/demo/app/stores/mismatch.ts
+++
@@ -1,9 +0,0 @@
-export const useMismatchStore = defineStore('mismatch', () => {
-  const name = ref('yugi')
-  const greeting = computed(() => `hi ${name.value}, it's ${Date.now()}`)
-
-  return {
-    name,
-    greeting,
-  }
-})
