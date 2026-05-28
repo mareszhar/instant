2a99db7a3a4a08af2c4d2d5c0c39ebdd8e4a1498
Mares <66750744+mareszhar@users.noreply.github.com>
2026-05-27 21:49:18 -0400

    ♻️ polish delete done tasks endpoint

--- client/packages/vux/idb-vux/demo/server/api/workspaces/[workspaceId]/tasks/done.delete.ts
+++ client/packages/vux/idb-vux/demo/server/api/workspaces/[workspaceId]/tasks/done.delete.ts
@@ -2,24 +2,17 @@ export default defineEventHandler(async (event) => {
   const workspaceId = expectWorkspaceId(event)
   const { userDb } = useIdbn(event, 'userDb!')
 
-  const [errorGettingWorkspaceData, workspaceQuery] = await go(userDb.query(q({
-    workspaces: {
+  const [errorQueryingTasksDone, tasksDoneQuery] = await go(userDb.query(q({
+    tasks: {
       $: {
-        where: { id: workspaceId },
+        where: { workspace: workspaceId, isDone: true },
         fields: ['id'],
-      },
-      tasks: {
-        $: {
-          where: { isDone: true },
-          fields: ['id'],
-          limit: 500,
-        },
+        limit: 500,
       },
     },
   })))
 
-  const workspace = workspaceQuery?.workspaces[0] ?? null
-  const tasksDone = workspace?.tasks ?? []
+  const tasksDone = tasksDoneQuery?.tasks ?? []
   const [errorDeletingTasksDone] = tasksDone.length > 0
     ? await go(userDb.transact(tasksDone.map(task => userDb.tx.tasks[task.id]!.delete())))
     : [undefined]
@@ -27,8 +20,8 @@ export default defineEventHandler(async (event) => {
   return {
     generatedAt: new Date().toISOString(),
     countOfTasksDoneDeleted: errorDeletingTasksDone ? 0 : tasksDone.length,
-    warning: (errorGettingWorkspaceData || errorDeletingTasksDone)
-      ? formatError(errorGettingWorkspaceData ?? errorDeletingTasksDone)
+    warning: (errorQueryingTasksDone || errorDeletingTasksDone)
+      ? formatError(errorQueryingTasksDone ?? errorDeletingTasksDone)
       : null,
   }
 })
