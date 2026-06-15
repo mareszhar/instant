64c5d47fb268d4ce4a5824d3d939cef155cf388c
Mares <66750744+mareszhar@users.noreply.github.com>
2026-06-15 02:38:18 -0400

    🔖 publish first release, v1.0.0

--- client/packages/dux/idb-dux/demo/config/nuxt/vite.ts
+++ client/packages/dux/idb-dux/demo/config/nuxt/vite.ts
@@ -1,14 +1,7 @@
 import type { NuxtConfig } from '@nuxt/schema'
 
 export const vite = {
-  // https://nuxt.com/docs/4.x/api/nuxt-config#vite
-  resolve: {
-    preserveSymlinks: true,
-  },
   optimizeDeps: {
-    exclude: [
-      '@mszr/idb-dux',
-    ],
     include: [
       '@floating-ui/vue',
       'go-go-try',
